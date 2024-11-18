215dea68cb09bce8048fb19f9c9eca8b28b7f869
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-19 04:01:12 +0600

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactStars from "react-rating-stars-component";
+import { Wave } from 'react-animated-text';
+import { Link } from 'react-router-dom';
 
 const Card = ({ brand }) => {
     console.log('Hello', brand);
-    const { brand_logo, brand_name, rating, description } = brand;
+    const { brand_logo, brand_name, rating, description, isSaleOn } = brand;
     return (
-        <div className='lg:w-8/12 grid grid-cols-2  px-4 lg:px-0 gap-4 lg:justify-between  items-center mx-auto py-8'>
+        <div className='lg:w-10/12 border border-green-500 grid grid-cols-2  p-4  gap-4 lg:justify-between  items-center  mx-auto my-8'>
             <div className="left">
                 <img className='w-40 h-40 lg:w-96 lg:h-96' src={brand_logo} alt="" />
-                <h2 className='text-xl'>{brand_name}</h2>
+                <h2 className='text-xl '>{brand_name}</h2>
 
 
                 <div className='flex items-center gap-3'>
@@ -26,21 +28,42 @@ const Card = ({ brand }) => {
 
             </div>
 
-            <div className="middle">
+            <div className="middle flex  justify-between">
 
-                <h2 className='text-xl'>{brand_name}</h2>
-                <h2 className='text-lg'>{description}</h2>
+                <div>
+                    <h2 className='text-xl font-bold'>{brand_name}</h2>
+                    <p className='py-3 text-lg'>{description}</p>
+                    
+                    <div className="lg:hidden py-3  text-green-600 ">
+                    
+                    {
+                        isSaleOn &&
 
+                        //  <button className='border border-green-300 bg-green-200 text-green-600 font-bold rounded-[32px] px-3 py-1'>  'sale is on'   </button>
+                        <Wave  text="sale is on"  />
+                    }
+                    </div>
+                    <Link  className='btn mt-3'>View Details</Link>
 
-                <div className="lg:hidden ">
-                    <button className='btn'>View Details</button>
+                    
+                
+                </div>
+
+                <div className=" hidden py-4 lg:block text-green-600 ">
+                    
+                    {
+                        isSaleOn &&
+
+                        //  <button className='border border-green-300 bg-green-200 text-green-600 font-bold rounded-[32px] px-3 py-1'>  'sale is on'   </button>
+                        <Wave  text="sale is on"  />
+                    }
                 </div>
 
 
             </div>
-            <div className="hidden lg:block right">
+            {/* <div className="hidden lg:block right">
                 <button className='btn'>View Details</button>
-            </div>
+            </div> */}
 
         </div>
     );
