840545d3ee9da47087888df5941d9961754e0499
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-18 16:38:31 +0600

    Navbar is okay

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     const { user, setUser } = useContext(AuthContext);
 
-    const links = <div className='flex md:text-lg flex-col md:flex-row gap-4 md:gap-10'>
+    const links = <div className='flex md:text-lg justify-center py-5 md:py-0 gap-5 md:gap-10'>
         <NavLink className='flex items-center gap-1' ><MdHome /> Home</NavLink>
         <NavLink className='flex items-center gap-1'  ><TbBrandAirtable /> Brand</NavLink>
          
@@ -33,8 +33,8 @@ const Navbar = () => {
         <div className=''>
             <div className="navbar text-white    bg-[#036544]">
                 <div className=" navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div className="">
+                        {/* <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-5 w-5"
@@ -47,17 +47,11 @@ const Navbar = () => {
                                     strokeWidth="2"
                                     d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content  text-white    bg-[#036544]   z-[1] mt-[47px] w-52 p-2 shadow">
-
-                            {links}
-
-                        </ul>
+                        </div> */}
+                        
                     </div>
                     {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
-                    <img className=" w-32 h-20 md:w-40 md:h-40 " src={logo} alt="logo" />
+                    <img className=" w-32 h-24 md:w-40 md:h-40 " src={logo} alt="logo" />
                 </div>
                 <div className="navbar-center hidden lg:flex">
 
@@ -108,6 +102,16 @@ const Navbar = () => {
 
 
                 }
+            </div>
+
+            <div className='text-white  lg:hidden   bg-[#036544]'>
+                
+               <div className=' '>
+                   {links}
+               </div>
+               
+               
+             
             </div>
         </div>
     );
